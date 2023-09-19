4648bcc9d1c32d3d818514e06314a774d8780afb
Addonay <addonay.osoro@student.moringaschool.com>
2023-09-19 22:21:08 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
+# app/app.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Create an instance of SQLAlchemy
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+# Initialize SQLAlchemy with the Flask app
+db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
